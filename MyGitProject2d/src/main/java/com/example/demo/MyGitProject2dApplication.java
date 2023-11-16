3090ef5d660a52d4af5a951c758b4c325c7ba68f
S0518 <S0518@21S0518>
2023-11-16 10:54:57 +0900

    test

---
+++ MyGitProject2d/src/main/java/com/example/demo/MyGitProject2dApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGitProject2dApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGitProject2dApplication.class, args);
+	}
+
+}
